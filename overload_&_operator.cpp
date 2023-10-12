4aae2f000e8772fc0979b743c138bd5a6628b6cb
siddhantkale <122144668+siddhantkale@users.noreply.github.com>
2023-10-12 15:26:44 +0530

    Create overload_&_operator.cpp

---
+++ overload_&_operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Numbers{
+public:
+	int a;
+  int operator&(Numbers num1){
+  return a|num1.a;
+  }
+};
+
+int main(){
+Numbers num1,num2;
+cout<<"Enter a number"<<endl;
+cin>>num1.a;
+cout<<"Enter second number"<<endl;
+cin>>num2.a;
+int num3 = num1&num2;
+cout<<"After performing operator overloading on & to get | the result is :"<<num3<<endl;
+return 0;
+}
